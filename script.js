070dbb10167bc006807a86a3f0c2472b63fe2ace
khubaib2310 <khubaibmuhammad964@gmail.com>
2022-08-20 01:36:05 +0500

    Final Protfolio

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-// function onClickMenu() {
-//   document.getElementById("menu").classList.toggle("icon");
-//   document.getElementById("nav").classList.toggle("change");
-// }
+function onClickMenu() {
+  document.querySelector(".burger-menu").style.display = "block";
+  document.querySelector(".cross").style.display = "block";
+  document.querySelector(".burger").style.display = "none";
+}
+
+function onClickMenu2() {
+  document.querySelector(".burger-menu").style.display = "none";
+  document.querySelector(".burger").style.display = "block";
+  document.querySelector(".cross").style.display = "none";
+}
+
+function defaultDisplay() {
+  if (window.innerWidth > 750) {
+    document.querySelector(".burger-menu").style.display = "none";
+    document.querySelector(".burger").style.display = "none";
+    document.querySelector(".cross").style.display = "none";
+  } else {
+    document.querySelector(".burger-menu").style.display = "none";
+    document.querySelector(".cross").style.display = "none";
+    document.querySelector(".burger").style.display = "block";
+  }
+}
+window.addEventListener("resize", defaultDisplay);
